b6769f4702dfffc84be5182384b4505b31333e2a
timkahlke <tim.kahlke@audiotax.is>
2017-03-17 12:46:31 +1100

    Initial commit

---
+++ addSampleQualByFastq.pl
@@ -0,0 +1,191 @@
+#!/usr/bin/perl
+
+#   This script is used to add a sample qualifier to a fasta file
+#   for further use use with vsearch
+#
+#
+#   COPYRIGHT DISCALIMER:
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#
+#   Author: Tim Kahlke, tim.kahlke@uts.edu.au
+#   Date: January 2017
+
+
+use strict;
+use Data::Dumper;
+use Getopt::Std;
+use Digest::MD5 qw(md5 md5_hex md5_base64);
+
+eval{
+    _main();
+};
+
+if($@){
+    print $@;
+}
+
+
+
+sub _main{
+    our %opts;
+    getopts("m:q:f:o:",\%opts);
+
+    my $fq = $opts{'q'};
+    my $fasta_in = $opts{'f'};
+    my $output = $opts{'o'};
+    my $m = $opts{'m'};
+
+    if(!$fq||!$output||!$fasta_in||!$m){
+        _usage();
+    }
+
+    ## Check that either a fastq file or directory of fastq files was given
+    my @files = ();
+    if(-f $fq){
+        @files = ($fq);
+    }
+    elsif(-d $fq){
+        opendir(DIR,$fq);
+        @files = map{"$fq/$_"}grep{($_=~/^.*_R1_.*/)||($_=~/^.*forward.*/)}grep{(($_=~/^.*\.fq/)||($_=~/^.*\.fastq/))}readdir(DIR);
+        if(!(scalar(@files))){
+            warn "No files ending on .fq or .fastq found in directory $fq";
+            _usage();
+        }
+    }
+    else{
+        warn "-q options is neither a file nor a directory!";
+        _usage();
+    }
+
+    # read mapping file of fastq to sample_id
+    print STDOUT "\n[STATUS]  Parsing mapping file\n";
+    my $map = _readMap($m);
+    print STDOUT "[STATUS]  Done parsing: ".scalar(keys(%$map))." samples/fastq files found in mapping file\n";
+
+    # check that all files in the mapping file are in the given directory
+    print STDOUT "\n[STATUS]  Checking mapping file and given input fastq files\n";
+    _checkMap($map,\@files);
+    print STDOUT "[STATUS]  Done\n";
+
+    # make lookup table of reads
+    print STDOUT "\n[STATUS] Parsing fastq files ...\n";
+    my ($rl,$ml) = _getLookup(\@files,$map);
+    print STDOUT "[STATUS] All fastq files parsed.\n";
+
+    # read input file and add sample qualifiers
+    open(my $ih,"<",$fasta_in) or die "Failed to open $fasta_in";
+    open(my $oh,">",$output) or die "Failed to open $output";
+    my $flag=0;
+    while(my $line=<$ih>){
+        if($line=~/^>.*$/){
+            my @ls = grep{$_ ne ""}split(/[>\t\s\n\r]/,$line);
+            my $key = $ls[0];
+            $key=~s/_/:/g;
+            if(exists($rl->{$key})){
+                $flag=1;
+                $line=">".$ls[0].";sample=".$rl->{$key}.";\n";
+            }
+            else{
+                $flag=0;
+            }
+        }
+        next unless $flag;
+        print $oh $line;
+    }
+    close($ih);
+    close($oh);
+    print STDOUT "[STATUS] Done!\n";
+
+}
+
+
+sub _getLookup{
+    my ($files,$map) = @_;
+
+    my $ml = [];
+    my $reads = {};
+    foreach my $f(@$files){
+        print STDOUT "[STATUS]\tParsing file $f\n"; 
+        my $i = scalar(@$ml);
+        my $fn = (split(/\//,$f))[-1];
+        die "No sample_id found for file $fn" unless $map->{$fn};
+        push @$ml, $map->{$fn};
+
+        open(my $ih,"<",$f) or die "Failed to open $f for reading.";
+        while(my $line=<$ih>){
+            if($.==1||!(($.-1)%4)){
+                my @ls = grep{$_ ne ""}split(/[@\s\t\n\r]/,$line);
+                $ls[0]=~s/_/:/g;
+                if($ls[0]=~/^(.*)\/[12]$/){
+                    $ls[0]=$1;
+                }
+                $reads->{$ls[0]} = $map->{$fn};
+            }
+        }
+        close($ih);
+    }
+    print STDOUT "[STATUS] Number of reads total: ".scalar(keys(%$reads))."\n";
+    return ($reads,$ml);
+}
+
+
+sub _readMap{
+    my $file = shift;
+    my $result = {};
+    open(my $ih,"<",$file) or die "Failed to open $file";
+    while(my $line=<$ih>){
+        my @ls = grep{$_ ne ""}split(/[\s\t\n]/,$line);
+        die "Unknown line format! More than two elements found in line $line!\nDoes your sample ID or fastq file include spaces or tabs?" unless
+        scalar(@ls)==2;
+        $result->{$ls[0]} = $ls[1];
+    }
+    close($ih);
+    return $result;
+}
+
+sub _checkMap{
+    my ($map,$files) = @_;
+
+    # check that all fastq files in mapping file are found in directory
+    foreach my $fm(keys(%$map)){
+        my $check = 0;
+        my $fns = [];
+        foreach my $f(@$files){
+            my $fn = (split(/\//,$f))[-1];
+            push @$fns, $fn;
+            next unless $fn eq $fm;
+            $check = 1;
+        }
+        if(!$check){
+            warn "\n\n[ERROR] Filename $fm in mapping file does not match any of the given fastq files\n\tFastq file names in mapping file:\n".join(",",keys(%$map))."\n\n\tNames of given fastq files:\n\t".join(",",@$fns)."\n\n";
+            _usage();
+        }
+    }
+}
+
+
+sub _usage{
+    print STDOUT "\n\nScript adds the sample qualifier to sequences based on the name of a given fastq file.\nSequences that can not be assigned a sample are removed from the output file.\n";
+    print STDOUT "Parameter:\n";
+    print STDOUT "q : fastq file or directory of fastq files\n";
+    print STDOUT "f : fasta file\n";
+    print STDOUT "o : output file\n";
+    print STDOUT "m : mapping file of fastq file name to sample_id.\n    One line per sample: first fastq file name then sample_id separated by tab or
+    space.\n";
+    print STDOUT "\n";
+    exit;
+}
+
---
+++ demultiplex.pl
@@ -0,0 +1,205 @@
+#!/usr/bin/perl
+#
+#   demultiplex.pl
+#
+#   Simple demultiplexing script for demultiplexing reads of structure:
+#   BARCODE (fw) - PRIMER (fw) - INSERT - PRIMER (rv) - BARCODE (rv) - ILLUMINA PRIMER
+#   
+#   The script demultiplexes by 
+#   1. concatenating the forward and reverse barcode-primer 
+#   2. Search for reads that start with forward concatenated sequence (100% match)
+#   3. Look for 100% match of reverse concatenated sequrnce.
+#   NOTE: Reverse sequence match can be ANYWHERE IN THE SEQUENCE! No positional
+#         information taken into account!
+#
+#   COPYRIGHT DISCALIMER:
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#   Author: Tim Kahlke, tim.kahlke@uts.edu.au
+#   Date: January 2017
+
+use strict;
+use Getopt::Std;
+use Data::Dumper;
+use warnings;
+
+eval{
+    _main();
+};
+
+if($@){
+    print $@;
+}
+
+
+sub _main{
+    our %opts;
+    getopts("i:o:m:cf",\%opts);
+    my $in = $opts{'i'};
+    my $m = $opts{'m'};
+    my $od = $opts{'o'};
+    my $clip = $opts{'c'};
+    my $fasta = $opts{'f'};
+
+    if(!$in||!$m||!$od){
+        _usage();
+    }
+
+    my $oligos = _readOligos($m);
+    my $ms = _getMatchSeqs($oligos);
+    _demux($od,$ms,$clip,$in,$fasta);
+
+    print STDOUT "\n\nDone. Output written to $od.\n\n";
+}
+        
+
+sub _getFHs{
+    my ($ms,$od,$fasta) = @_;
+    my $fhs = {};
+   
+    foreach my $s(keys(%$ms)){
+        open(my $fh,">","$od/$s.fastq") or die "Failed to open $od/$s.fastq for writing";
+        $fhs->{'fastq'}->{$s} = $fh;
+        if($fasta){
+            open(my $fh,">","$od/$s.fasta") or die "Failed to open $od/$s.fasta for writing.";
+            $fhs->{'fasta'}->{$s} = $fh;
+        }
+    }
+    return $fhs;
+}
+
+
+sub _close{
+    my $fhs = shift;
+    foreach my $k(keys(%$fhs)){
+        foreach(keys(%{$fhs->{$k}})){
+            close($fhs->{$k}->{$_});
+        }
+    }
+}
+
+
+sub _demux{
+    my ($od,$ms,$clip,$in,$fasta) = @_;
+
+    my $fhs = _getFHs($ms,$od,$fasta);
+
+    open(my $ih,"<",$in) or die "Failed to open file $in for reading.";
+    print STDOUT "\n[STATUS] Parsing file $in\n";
+    while(!(eof $ih)){
+        my $sid = readline($ih);
+        my $seq = readline($ih);
+        my $plus = readline($ih);
+        my $qual = readline($ih);
+
+        if(!(($./4)%10000)){
+            print STDOUT "\t$. lines parsed\n";
+        }
+
+        foreach my $s(keys(%$ms)){
+            my $f = $ms->{$s}->{'forward'};
+            my $r = $ms->{$s}->{'reverse'};
+
+            if($seq=~/^$f(.*)$r(.*)$/){
+                my $insert = $1;
+                my $suffix = $2;
+                if($clip){
+                    $seq=$insert."\n";
+                    $qual = substr($qual,length($f),length($insert))."\n";
+                }
+                my $oh = $fhs->{'fastq'}->{$s};
+                print $oh "$sid$seq$plus$qual";
+                if($fasta){
+                    my $fh = $fhs->{'fasta'}->{$s};
+                    $sid=~s/@//;
+                    print $fh ">$sid$seq";
+                }
+                last;
+            }
+        }
+    }
+    close($ih);
+    _close($fhs);
+}
+
+
+sub _getMatchSeqs{
+    my $oligos = shift;
+    my $ms = {};
+
+    my $p3 = $oligos->{'p3'}?$oligos->{'p3'}:"";
+    warn "[WARNING] No P3 Illumina primer found! Using empty string instead!" unless $p3;
+
+    my $fp = $oligos->{'primer'}->{'forward'}?$oligos->{'primer'}->{'forward'}:"";
+    warn "[WARNING] No forward primer found! Using empty string instead" unless $fp;
+
+    my $rp = $oligos->{'primer'}->{'reverse'}?$oligos->{'primer'}->{'reverse'}:"";
+    warn "[WARNING] No reverse primer found! Using empty string instead" unless $rp;
+
+    foreach my $k(keys(%{$oligos->{'barcode'}})){
+        $ms->{$k}->{'forward'} = $oligos->{'barcode'}->{$k}->{'forward'}.$fp;
+        $ms->{$k}->{'reverse'} = $rp.$oligos->{'barcode'}->{$k}->{'reverse'}.$p3;
+    }
+    return $ms;
+}
+
+
+
+sub _readOligos{
+    my $file = shift;
+
+    my $oligos = {};
+    open(my $ih,"<",$file) or die "Failed to open $file for reading";
+    while(my $line=<$ih>){
+        my @ls = grep{$_ ne ""}split(/[\s\t\n\r]/,$line);
+        if($line=~/^primer.*$/){
+            $oligos->{'primer'} = {forward=>$ls[1],reverse=>$ls[2]};
+        }
+        elsif($line=~/^barcode.*$/){
+            $oligos->{'barcode'}->{$ls[-1]} = {forward=>$ls[1],reverse=>$ls[2]};
+        }
+        elsif($line=~/^p3.*$/){
+            $oligos->{'p3'} = $ls[1];
+        }
+        else{
+            warn "Unknown foramt of line! Ignoring line $line";
+        }
+    }
+    close($ih);
+    return $oligos;
+}
+
+
+sub _usage{
+    print STDOUT "\n\nBasic demultiplexing tool using exact matches on dual-barcode single-end files.\n";
+    print STDOUT "Parameter:\n";
+    print STDOUT "i : input fastq file\n";
+    print STDOUT "m : mothur oligos file for primer and barcode\n";
+    print STDOUT "    Format of mapping file:\n";
+    print STDOUT "    primer FORWARD BACKWARD\n";
+    print STDOUT "    p3 additional primer, e.g. p3_ILLUMINA_PRIMER\n";
+    print STDOUT "    barcode FORWARD BACKWARD\n";
+    print STDOUT "    ...\n\n";
+    print STDOUT "o : directory for output files\n";
+    print STDOUT "c : if set primer and barcodes will be clipped from reads\n";
+    print STDOUT "f : if set fasta files will be written out, too.\n";
+    print STDOUT "\n\n";
+    exit;
+}
+
+
+
+
+
